88ce860c69a09d582f9395366b878db47e445af8
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-02-20 13:05:44 -0600

    got ternary working for user data

--- src/Home.tsx
+++ src/Home.tsx
@@ -34,11 +34,13 @@ export const Home: FC<HomeProps> = ({ leaderboardData }) => {
                         Leaderboard
                     </h2>
                     {
-                        leaderboardData.map(x => (
-                            <p>
-                                {x.name}
-                            </p>
-                        ))
+                        leaderboardData.length > 0
+                            ? leaderboardData.map(x => (
+                                <p>
+                                    {x.name}
+                                </p>
+                            ))
+                            : (<p> Play a game to see the leaderboard!</p>)
                     }
                 </div>
             </div>
