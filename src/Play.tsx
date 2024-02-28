1dec7f52071b6a33ec403e4d1b91c5ddb1778a02
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-02-27 19:59:34 -0600

    Update Play.tsx

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,23 +1,47 @@
-import { useNavigate} from 'react-router-dom' ;
+import { useNavigate } from 'react-router-dom';
 
 export const Play = () => {
-  
+
   const nav = useNavigate();
-  
+
   return (
-      <>
-        <h3>
-          Play
-        </h3>
-        <p>
-          Play the game and tap-a-tap-a-tap
-        </p>
-        <button
-            className="btn btn-outline btn-primary"
-            onClick={() => nav(-2)}
-        >
-            Done
-        </button>
-      </>
-    );
-  };
+    <>
+      <h1>
+        Play Trash Pandas
+      </h1>
+      <p>
+        Play the game and tap-a-tap-a-tap
+      </p>
+
+      <div className="card-bordered w-96 bg-base-100  shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title underline">Player 1</h2>
+          {/* Need to make the card put the player name and copy cart for each player is this going to make it too complicated??? */}
+          <div className="  card-actions justify-center p-2">
+            <p className="self-center">Roll Count</p>
+            <button className="btn btn-error self-center">-</button>
+            <p className="p-3 self-center border-b-2">#</p>
+            <button className="btn btn-success">+</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+          <div className="  card-actions justify-center p-2">
+            <button className="btn btn-outline btn-warning self-center">Forced Roll</button>
+            <button className="btn btn-outline btn-error self-center">BUST!</button>
+            <button className="btn btn-outline btn-success">End Turn</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+        </div>
+
+      </div >
+
+      <button
+        className="btn btn-outline btn-primary"
+        onClick={() => nav(-2)}
+      >
+        Done
+      </button>
+    </>
+  );
+};
