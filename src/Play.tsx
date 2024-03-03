3acf3a94ef9c87d385804c45aa109a36571aec50
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-03-03 11:06:35 -0600

    updated play screen with my play card

--- src/Play.tsx
+++ src/Play.tsx
@@ -21,6 +21,28 @@ export const Play: FC<PlayProps> = ({ addNewGameResult, setTitle }) => {
     <div
       className='flex flex-col gap-3'
     >
+      <div className="card-bordered w-96 bg-base-200  shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title underline">Player 1</h2>
+          {/* Need to make the card put the player name and copy cart for each player is this going to make it too complicated??? */}
+          <div className="  card-actions justify-center p-2">
+            <p className="self-center">Roll Count</p>
+            <button className="btn btn-error self-center">-</button>
+            <p className="p-3 self-center border-b-2">#</p>
+            <button className="btn btn-success">+</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+          <div className="  card-actions justify-center p-2">
+            <button className="btn btn-outline btn-warning self-center">Forced Roll</button>
+            <button className="btn btn-outline btn-error self-center">BUST!</button>
+            <button className="btn btn-outline btn-success">End Turn</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+        </div>
+
+      </div >
 
       <button
         className="btn btn-lg btn-primary"
