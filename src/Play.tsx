a976f8d92317edfd7c0684673c862fd8dc5ebfc8
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-03-10 09:02:09 -0500

    scope change to Play Screen

--- src/Play.tsx
+++ src/Play.tsx
@@ -35,8 +35,31 @@ export const Play: FC<PlayProps> = ({ addNewGameResult, setTitle }) => {
           </div>
 
           <div className="  card-actions justify-center p-2">
-            <button className="btn btn-outline btn-warning self-center">Forced Roll</button>
-            <button className="btn btn-outline btn-error self-center">BUST!</button>
+            {/* <button className="btn btn-outline btn-warning self-center">Forced Roll</button> */}
+            <button className="btn btn-outline btn-error self-center">BUST End Turn</button>
+            <button className="btn btn-outline btn-success">End Turn</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+        </div>
+
+      </div >
+
+      <div className="card-bordered w-96 bg-base-200  shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title underline">Player 2</h2>
+          {/* Need to make the card put the player name and copy cart for each player is this going to make it too complicated??? */}
+          <div className="  card-actions justify-center p-2">
+            <p className="self-center">Roll Count</p>
+            <button className="btn btn-error self-center">-</button>
+            <p className="p-3 self-center border-b-2">#</p>
+            <button className="btn btn-success">+</button>
+            {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
+          </div>
+
+          <div className="  card-actions justify-center p-2">
+            {/* <button className="btn btn-outline btn-warning self-center">Forced Roll</button> */}
+            <button className="btn btn-outline btn-error self-center">BUST End Turn</button>
             <button className="btn btn-outline btn-success">End Turn</button>
             {/* the p above is placeholder where I Will display # of rolls. also need to fix spacing  */}
           </div>
@@ -60,7 +83,7 @@ export const Play: FC<PlayProps> = ({ addNewGameResult, setTitle }) => {
           nav(-2)
         }}
       >
-        Done
+        Done - End Game
       </button>
       <p className='text-xs'>
         Play the game and tap-a-tap-a-tap
