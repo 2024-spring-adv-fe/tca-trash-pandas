3a8e29a8e282d11c78be54aaad3cb32611a130d3
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-04-16 18:25:41 -0500

    Fix my branch merge errors. DUMMY

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,7 @@ interface HomeProps {
     leaderboardData: LeaderboardEntry[];
     setTitle: (t: string) => void;
     generalFacts: GeneralFacts;
-    pointFunFacts: PointFunFacts;
+    pointFunFacts: PointFunFacts[];
 }
 
 export const Home: FC<HomeProps> = ({
@@ -98,67 +98,28 @@ export const Home: FC<HomeProps> = ({
             <div className='card mt-3 bg-base-200 shadow-xl m-1.5'>
                 <div className='card-body'>
                     <h2 className='card-title'>
-                        Average Points / Turn
+                        Average Points / Game
                     </h2>
-                    <table className='table '>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th># Points</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Melisa</td>
-                                <td>52</td>
-                            </tr>
-                            <tr>
-                                <td>Hailey</td>
-                                <td>43.75</td>
-                            </tr>
-                            <tr>
-                                <td>Katlyn</td>
-                                <td>48.25</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-            <div className='card mt-3 bg-base-200 shadow-xl m-1.5'>
-                <div
-                    className='card-body p-3'
-                >
-                    <h2
-                        className='card-title'
-                    >
-                        Best/Worst Players
-                    </h2>
-                    {
-                        pointFunFacts.maxPointPlayers.length > 0
-                            ? (
-                                <table
-                                    className='table'
-                                >
-                                    <tbody>
-                                        <tr>
-                                            <td>Most Points</td>
-                                            <td className='text-right'>{pointFunFacts.maxPointValue}</td>
-                                            <td>{pointFunFacts.maxPointPlayers}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Fewest Points</td>
-                                            <td className='text-right'>{pointFunFacts.minPointValue}</td>
-                                            <td>{pointFunFacts.minPointPlayers}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            )
-                            : (
-                                <p>
-                                    Play a game to see best/worst players!
-                                </p>
-                            )
+                    {pointFunFacts.length > 0
+                        ? (<table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>Player</th>
+                                    <th>Average Points</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {pointFunFacts.map(pff => (
+                                    <tr key={pff.name}                                    >
+                                        <td>{pff.name}</td>
+                                        <td>{pff.avg.toFixed(3)}</td>
+                                    </tr>
+                                ))
+                                }
+                            </tbody>
+                        </table>
+                        )
+                        : (<p> Play a game to see the leaderboard!</p>)
                     }
                 </div>
             </div>
